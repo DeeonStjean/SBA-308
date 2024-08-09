2fca12b84d84cd1abf1551de1541796c9c068ae5
DeeonStjean <deeonstjean101@gmail.com>
2024-08-08 22:27:16 -0400

    adding function call SubmitOnTime

--- script.js
+++ script.js
@@ -77,36 +77,59 @@ const AssignmentGroup = {
 ];
   
 function getLearnerData(course, ag, submissions) {
-    // here, we would process this data to achieve the desired result.
-    try {
-        if(ag.course_id!=course.id){
-            throw "Error: assignment course id don't match course id"
+  // here, we would process this data to achieve the desired result.
+  try {
+    if(ag.course_id!=course.id){
+      throw "Error: assignment course id don't match course id"
+    }
+    else{
+      let learnerID1=[];
+      const learnerID2=[];
+      for(let i=0;i<submissions.length;i++){
+        if(submissions[i].learner_id==125){
+          learnerID1.push(submissions[i])
+        }else{
+          learnerID2.push(submissions[i])
         }
-        else{
+      }
+      //console.log(learner1);
 
-
-            const result = [
-                {
-                    id: 125,
-                    avg: 0.985, // (47 + 150) / (50 + 150)
-                    1: 0.94, // 47 / 50
-                    2: 1.0 // 150 / 150
-                },
-                {
-                id: 132,
-                avg: 0.82, // (39 + 125) / (50 + 150)
-                1: 0.78, // 39 / 50
-                2: 0.833 // late: (140 - 15) / 150
-              }
-            ];
-            return result;
-        }
+      const result = [
+      {
+        id: 125,
+        avg: 0.985, // (47 + 150) / (50 + 150)
+        1: 0.94, // 47 / 50
+        2: 1.0 // 150 / 150
+      },
+      {
+        id: 132,
+        avg: 0.82, // (39 + 125) / (50 + 150)
+        1: 0.78, // 39 / 50
+        2: 0.833 // late: (140 - 15) / 150
+      }
+      ];
+      return result;
+    }
     } catch (error) {
         console.log(error);
     }
     //return result;
 }
+
+function average(ag,sub){
   
+}
+function SubmitOnTime(ag,sub){
+  const assDate= new Date(ag);
+  const subDate=new Date(sub);
+  const today=new Date();
+  if(assDate<today && assDate>=subDate ){//check if assignment was hand in before due date or on due date
+    return true;
+  }
+  else
+    return false;
+}
 const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
   
 console.log(result);
+//console.log(LearnerSubmissions.length);
