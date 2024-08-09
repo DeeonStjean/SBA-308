3c02498bef25172d5df52d895ced037951c0cc56
DeeonStjean <deeonstjean101@gmail.com>
2024-08-09 03:10:36 -0400

    add a working function call average

--- script.js
+++ script.js
@@ -83,29 +83,65 @@ function getLearnerData(course, ag, submissions) {
       throw "Error: assignment course id don't match course id"
     }
     else{
+      let today = new Date();
+      let notdue=false;
+      let notdueDate;
+
+      while(notdue==false){
+        for(let i=0;i<ag.assignments.length;i++){
+          let dueDate=new Date(ag.assignments[i].due_at);
+          if(dueDate>today){
+            notdueDate=dueDate;
+            notdue=true;
+          }
+        }
+      }
+
       let learnerID1=[];
-      const learnerID2=[];
-      for(let i=0;i<submissions.length;i++){
+      let learnerID2=[];
+      for(let i=0;i<submissions.length;i++){//slipt the array to two array one for id 125 and id 132
         if(submissions[i].learner_id==125){
           learnerID1.push(submissions[i])
         }else{
           learnerID2.push(submissions[i])
         }
       }
-      //console.log(learner1);
 
+      let avgByAss1=[];
+      let avgByAss2=[];
+
+      for(let i=0;i<learnerID1.length;i++){
+         let assignmentsDate = new Date(ag.assignments[i].due_at);
+        if(today<assignmentsDate)
+          break;
+        else{
+          avgByAss1.push(average(ag.assignments[i].points_possible,learnerID1[i].submission.score,
+          SubmitOnTime(ag.assignments[i].due_at,learnerID1[i].submission.submitted_at)));
+        }
+        
+      }
+      for(let i=0;i<learnerID2.length;i++){
+        let assignmentsDate = new Date(ag.assignments[i].due_at);
+       if(today<assignmentsDate)
+         break;
+       else{
+         avgByAss2.push(average(ag.assignments[i].points_possible,learnerID2[i].submission.score,
+         SubmitOnTime(ag.assignments[i].due_at,learnerID2[i].submission.submitted_at)));
+       }
+       
+     }
       const result = [
       {
-        id: 125,
+        id: learnerID1[0].learner_id,
         avg: 0.985, // (47 + 150) / (50 + 150)
-        1: 0.94, // 47 / 50
-        2: 1.0 // 150 / 150
+        1: avgByAss1[0], // 47 / 50 = 0.94
+        2: avgByAss1[1] // 150 / 150 = 1
       },
       {
-        id: 132,
+        id: learnerID2[0].learner_id,
         avg: 0.82, // (39 + 125) / (50 + 150)
-        1: 0.78, // 39 / 50
-        2: 0.833 // late: (140 - 15) / 150
+        1: avgByAss2[0], // 39 / 50 = .78
+        2: avgByAss2[1] // late: (140 - 15) / 150 = .0833
       }
       ];
       return result;
@@ -116,20 +152,30 @@ function getLearnerData(course, ag, submissions) {
     //return result;
 }
 
-function average(ag,sub){
-  
+function average(ag,sub,sot){
+  let totalPoint=ag;
+  let score=sub;
+  if(totalPoint==0){
+    throw "Error Can't divide by zero(0) "
+  }
+  else if(sot==true){
+    return score/totalPoint;
+  }else{
+    let late=totalPoint/10;
+    return (score-late)/totalPoint;
+  }
 }
-function SubmitOnTime(ag,sub){
+function SubmitOnTime(ag,sub){//check if assignment was hand in before due date or on due date or late
   const assDate= new Date(ag);
   const subDate=new Date(sub);
   const today=new Date();
-  if(assDate<today && assDate>=subDate ){//check if assignment was hand in before due date or on due date
+  if(assDate<today && assDate>=subDate ){//hand in on time return true
     return true;
   }
-  else
+  else//hand in late return false 
     return false;
 }
+
 const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
   
 console.log(result);
-//console.log(LearnerSubmissions.length);
